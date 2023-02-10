8ef02cb72e866940d1f1a9661eafea68a3f4c503
elis1386 <elis1386@gmail.com>
2023-02-10 14:20:44 +0200

    add styles and colors

--- src/app/client-tasks/client-tasks.component.ts
+++ src/app/client-tasks/client-tasks.component.ts
@@ -5,10 +5,11 @@ import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-client-tasks',
   templateUrl: './client-tasks.component.html',
-  styleUrls: ['./client-tasks.component.css'],
+  styleUrls: ['./client-tasks.component.css']
 })
 export class ClientTasksComponent {
-  date: any;
+date: any;
+
 }
 
 export class NgbdAccordionStatic {}
