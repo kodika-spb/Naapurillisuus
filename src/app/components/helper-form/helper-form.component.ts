ea770de07549bc1cc22a415badd4f91b14a8994d
Ваня Дубинников <ivan.dubinnikov@gmail.com>
2023-02-15 11:17:23 +0200

    Make an authorithaition valid, change a forms and add to auth service and guard methods for verification users

--- src/app/components/helper-form/helper-form.component.ts
+++ src/app/components/helper-form/helper-form.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { HelperFormService } from '../../services/helper-form.service';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-helper-form',
@@ -10,7 +13,34 @@ import { HelperFormService } from '../../services/helper-form.service';
 export class HelperFormComponent {
 
   form: any;
+  signupForm: FormGroup;
+  firebaseErrorMessage: string;
 
-  constructor(public helperformService: HelperFormService){}
+  constructor(private afAuth: AngularFireAuth, private authService: AuthService, private router: Router, public helperformService: HelperFormService){
+    this.firebaseErrorMessage = '';
+  }
+
+  ngOnInit():void{
+    this.signupForm = new FormGroup({
+      'firstName': new FormControl ('', Validators.required),
+      'lastName': new FormControl ('', Validators.required),
+      'email': new FormControl ('', [Validators.required, Validators.email]),
+      'password': new FormControl ('', Validators.required)
+    });
+  }
+
+  signup(){
+    if(this.signupForm.invalid)
+      return;
+
+      this.authService.signupUser(this.signupForm.value).then((result) => {
+        if(result == null)
+          this.router.navigate(['/helper-dashboard']);
+        else if(result.isValid = false)
+          this.firebaseErrorMessage = result.message;
+      }).catch(() => {
+
+      });
+  }
 
 }
--- src/app/components/sign-in/sign-in.component.ts
+++ src/app/components/sign-in/sign-in.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
 
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire//compat/auth';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+
+
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent {
+  firebaseErrorMessage: string;
+  loginForm: FormGroup;
+
+  constructor(private afAuth: AngularFireAuth, private authService: AuthService, private router: Router){
+    this.firebaseErrorMessage = '';
+    this.loginForm = new FormGroup({
+      'email': new FormControl ('', [Validators.required, Validators.email]),
+      'password': new FormControl ('', Validators.required)
+    });
+  }
+  login(){
+    if(this.loginForm.invalid)
+      return;
+
+      this.authService.loginUser(this.loginForm.value.email, this.loginForm.value.password).then((result) => {
+        if(result == null){
+          console.log('login into...');
+          this.router.navigate(['/helper-dashboard']);
+        }
+        else if(result.isValid = false){
+          console.log('page reloaded')
+          this.firebaseErrorMessage = result.message;
+        }
+      }).catch(() => {
+
+      });
+  }
 
 }
