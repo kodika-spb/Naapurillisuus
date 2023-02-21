a0b720bbbeb29bf5e392d7b4494540e944e491dc
elis1386 <elis1386@gmail.com>
2023-02-21 15:45:47 +0200

    add tasks.ts file with default tasks

--- src/app/components/helper-dashboard/helper-dashboard.component.ts
+++ src/app/components/helper-dashboard/helper-dashboard.component.ts
@@ -1,17 +1,22 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgIf } from '@angular/common';
+import { VTask } from 'src/app/models/vtasks';
+import { tasks as data } from 'src/app/data/tasks';
 
 @Component({
   selector: 'app-helper-dashboard',
   templateUrl: './helper-dashboard.component.html',
   styleUrls: ['./helper-dashboard.component.css'],
 })
-export class HelperDashboardComponent {
+export class HelperDashboardComponent implements OnInit {
   imgUrl = './assets/myAcc4.png';
   disabled = false;
   myQrCode: boolean = false;
+  tasks: VTask[] = data;
 
-  constructor(){}
+  constructor() {}
 
+  ngOnInit(): void {}
+ 
 }
